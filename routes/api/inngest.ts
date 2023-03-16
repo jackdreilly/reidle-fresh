e6433ec6b82bfa3d8122cbe952943d400b9701dc
Jack Reilly <jackdreilly@gmail.com>
2023-03-16 09:42:43 +0100

    asdf

--- routes/api/inngest.ts
+++ routes/api/inngest.ts
@@ -3,13 +3,22 @@ import { serve } from "inngest/deno/fresh";
 
 const inngest = new Inngest({ name: "My app" });
 
-const fn = inngest.createFunction(
+const helloWorld = inngest.createFunction(
+  { name: "Hello World" },
+  { event: "test/hello.world" },
+  async ({ event, step }) => {
+    await step.sleep("1s");
+    return { event, body: "Hello, World!" };
+  },
+);
+
+const email = inngest.createFunction(
   { name: "Weekly digest email" },
   { cron: "TZ=UTC * * * * *" },
-  async () => {
-    
+  async ({ event, step }) => {
+    return { event, body: "yo!" };
   },
 );
 
 // Serve the Inngest API.
-export const handler = serve(inngest , [fn]);
+export const handler = serve(inngest, [helloWorld, email]);
