7da7fca7bc418738e2dbda64c53bb3f0c0c6b5f8
Jack Reilly <jackdreilly@gmail.com>
2023-03-20 14:54:24 +0100

    notifications

--- islands/ReadNotification.tsx
+++ islands/ReadNotification.tsx
@@ -7,5 +7,12 @@ export default function ReadNotification() {
       setUnread(x)
     );
   }, []);
-  return <div class="contents">{unread ? "🟢" : ""}</div>;
+  return (
+    <span class={"m-1 relative h-3 w-3 " + (unread ? "inline-flex" : "hidden")}>
+      <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-[#38bdf8] opacity-75">
+      </span>
+      <span class="relative inline-flex rounded-full h-3 w-3 bg-[#0ea5e9]">
+      </span>
+    </span>
+  );
 }
--- islands/drawer.tsx
+++ islands/drawer.tsx
@@ -1,27 +1,32 @@
 import { reidleHeaderLinks } from "@/components/reidle_template.tsx";
 import { useState } from "preact/hooks";
+import ReadNotification from "./ReadNotification.tsx";
 
 export default function Drawer() {
   const [show, setShow] = useState(false);
   return (
-    <div>
-      <svg
-        onClick={() => setShow((s) => !s)}
-        class="h-5 w-5"
-        fill="#888888"
-        version="1.1"
-        id="Capa_1"
-        xmlns="http://www.w3.org/2000/svg"
-        width="800px"
-        height="800px"
-        viewBox="0 0 24.75 24.75"
-      >
-        <g>
-          <path d="M0,3.875c0-1.104,0.896-2,2-2h20.75c1.104,0,2,0.896,2,2s-0.896,2-2,2H2C0.896,5.875,0,4.979,0,3.875z M22.75,10.375H2
+    <div onClick={() => setShow((s) => !s)}>
+      <div class="relative">
+        <svg
+          class="h-5 w-5"
+          fill="#888888"
+          version="1.1"
+          id="Capa_1"
+          xmlns="http://www.w3.org/2000/svg"
+          width="800px"
+          height="800px"
+          viewBox="0 0 24.75 24.75"
+        >
+          <g>
+            <path d="M0,3.875c0-1.104,0.896-2,2-2h20.75c1.104,0,2,0.896,2,2s-0.896,2-2,2H2C0.896,5.875,0,4.979,0,3.875z M22.75,10.375H2
        c-1.104,0-2,0.896-2,2c0,1.104,0.896,2,2,2h20.75c1.104,0,2-0.896,2-2C24.75,11.271,23.855,10.375,22.75,10.375z M22.75,18.875H2
        c-1.104,0-2,0.896-2,2s0.896,2,2,2h20.75c1.104,0,2-0.896,2-2S23.855,18.875,22.75,18.875z" />
-        </g>
-      </svg>
+          </g>
+        </svg>
+        <div class="bottom-2 left-2 absolute">
+          <ReadNotification />
+        </div>
+      </div>
       <nav
         class={(show ? "block" : "hidden") +
           " absolute top-[55px] shadow-2xl rounded-md right-1 w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg"}
@@ -54,6 +59,7 @@ export default function Drawer() {
                 ].join(" ")}
               >
                 {text}
+                {text === "Messages" ? <ReadNotification /> : null}
               </a>
             </li>
           ))}
