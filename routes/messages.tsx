33a7623b47fa8a0d5d8400c0674dbe878f36fe6c
Jack Reilly <jackdreilly@gmail.com>
2023-03-21 15:50:26 +0100

    messages better

--- routes/messages.tsx
+++ routes/messages.tsx
@@ -58,6 +58,10 @@ SELECT
     "id"
 FROM
     "messages"
+WHERE
+    created_at::DATE
+      >=
+      (CURRENT_DATE - INTERVAL '1' MONTH)
 ORDER BY
     "created_at"
         DESC
@@ -72,7 +76,7 @@ export default function Page(
 ) {
   return (
     <ReidleTemplate route="/messages" title="Messages">
-      <ul class="overflow-scroll" style={{ height: "calc(90vh - 5em)" }}>
+      <ul class="overflow-scroll" style={{ height: "calc(100vh - 13em)" }}>
         {messages.map(({ message, id, name, created_at }, i) => (
           <li class="border-b-1 p-2">
             <span class="font-bold">{name}</span>: {message}
@@ -99,9 +103,8 @@ export default function Page(
         ))}
       </ul>
       <div class="mt-3">
-        <form method="POST" class="flex">
+        <form method="POST" class="flex h-[5em]">
           <textarea
-            rows={2}
             required
             class="flex-grow-1 break-word mr-2.5 p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
             placeholder="Your message..."
