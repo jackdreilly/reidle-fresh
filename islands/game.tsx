376e3a4935d70ad8f1ab0a163b7655d052b6a0a5
Jack Reilly <jackdreilly@gmail.com>
2023-03-22 08:01:49 +0100

    key shit

--- islands/game.tsx
+++ islands/game.tsx
@@ -386,7 +386,7 @@ export default function Game(
                       ? (
                         <svg
                           class="p-1 m-auto"
-                          style={{ maxWidth: "40px" }}
+                          style={{ maxWidth: "35px" }}
                           fill="currentColor"
                           viewBox="0 0 20 20"
                           xmlns="http://www.w3.org/2000/svg"
@@ -397,6 +397,20 @@ export default function Game(
                             fillRule="evenodd"
                             d="M7.22 3.22A.75.75 0 017.75 3h9A2.25 2.25 0 0119 5.25v9.5A2.25 2.25 0 0116.75 17h-9a.75.75 0 01-.53-.22L.97 10.53a.75.75 0 010-1.06l6.25-6.25zm3.06 4a.75.75 0 10-1.06 1.06L10.94 10l-1.72 1.72a.75.75 0 101.06 1.06L12 11.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L12 8.94l-1.72-1.72z"
                           />
+                        </svg>
+                      )
+                      : c === "↵"
+                      ? (
+                        <svg
+                          class="p-1 m-auto"
+                          style={{ maxWidth: "35px" }}
+                          fill="currentColor"
+                          viewBox="0 0 1200 1200"
+                          xmlns="http://www.w3.org/2000/svg"
+                          aria-hidden="true"
+                        >
+                          <path d="M808.969,133.929v257.06H942.94v267.899H417.981V508.763L0,787.417
+                     l417.982,278.654V915.946h524.959H1200V658.888V390.988v-257.06H942.941H808.969L808.969,133.929z" />
                         </svg>
                       )
                       : c}
