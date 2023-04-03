a5a68eb684abc6093b3ea03e3c025263d813f196
Jack Reilly <jackdreilly@gmail.com>
2023-04-03 10:14:53 +0200

    better even

--- islands/confetti.tsx
+++ islands/confetti.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "preact/hooks";
-const stepMS = 1;
-const nConfetti = 80;
+const stepMS = 25;
+const nConfetti = 200;
+const factor = 1e1;
 class State {
   step(): State {
     for (const confetti of this.confetti) {
@@ -89,39 +90,43 @@ class ConfettiPiece {
     public rotationVelocity: Vector,
     public hsl: Hsl,
     public minVelocity: number,
+    public height: number,
   ) {}
   stepForward() {
     this.position = this.position.add(this.velocity);
     this.rotation = this.rotation.add(this.rotationVelocity);
     this.velocity.y = Math.max(
       this.minVelocity,
-      this.velocity.y - 20e-5 - Math.abs(this.velocity.y) * .8e-1,
+      this.velocity.y - 80e-4 - Math.abs(this.velocity.y) * 1.5e-1,
     );
     this.velocity.x += rand() * 5e-5;
     this.velocity.x *= .99;
     this.rotationVelocity = this.rotationVelocity.mul(.99999);
   }
   static randomStart() {
-    const rotationVelocity = Vector.random().add(1).norm().mul(1e-1);
+    const rotationVelocity = Vector.random().add(1).norm().mul(2);
+    rotationVelocity.y *= 2e-1;
     const rotation = Vector.random().norm().mul(1e3);
-    rotation.x *= 6e-2;
     const velocity = new Vector(0, 1, 0).add(Vector.random().mul(1e-2))
       .rotateXY(
         rand() * 3,
-      ).norm().mul((1 + rand() * 1e-1) * 7.5e-2);
+      ).norm().mul((1 + rand() * 1e-1) * 7.5e-2 * 3.2);
     const hsl = new Hsl(Math.random() * 360, 50, 50);
     const position = new Vector(0.5 + rand() * 1e-3, 0, 1 + rand() * 3e-1);
+    const minVelocity = -1e-3 * (1 + rand() * 0.4) * factor;
+    const height = (7e-2 + rand() * 6e-2) * 4e-1;
     return new ConfettiPiece(
       position,
       velocity,
       rotation,
       rotationVelocity,
       hsl,
-      -3e-3 * (1 + rand() * 0.4),
+      minVelocity,
+      height,
     );
   }
   svg() {
-    const { position, rotation, hsl } = this;
+    const { position, rotation, hsl, height } = this;
     const [x, y, z] = [position.x, position.y, position.z];
     const [rx, ry, rz] = [rotation.x, rotation.y, rotation.z];
     return (
@@ -134,11 +139,11 @@ class ConfettiPiece {
         <g
           transform={`rotate(${rx} ${
             (Math.abs(Math.cos(ry * 1e-0)) * 8e-3 + 1e-3) * 0.5
-          } 4e-2)`}
+          } ${height / 2})`}
         >
           <rect
             width={Math.abs(Math.cos(ry * 1e-0)) * 8e-3 + 1e-3}
-            height={8e-2}
+            height={height}
             x="0"
             y="0"
             fill={`hsl(${hsl.h} ${hsl.s}% ${hsl.l}%)`}
