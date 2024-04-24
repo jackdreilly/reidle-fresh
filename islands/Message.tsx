9adb4dfdb20f0da095f63878bb557ce6d5999a3b
Jack Reilly <jackdreilly@gmail.com>
2024-04-23 20:09:34 -0700

    make links links

--- islands/Message.tsx
+++ islands/Message.tsx
@@ -96,7 +96,7 @@ function MaybeImage({ url }: { url: string }) {
         }}
       />
     )
-    : <>{url}</>;
+    : <a href={url}>{url}</>;
 }
 
 export default function Message({ message }: { message: string }) {
